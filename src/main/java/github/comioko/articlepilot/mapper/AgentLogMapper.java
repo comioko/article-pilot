4975a9a653d25dcb596e1ae1419c924ef2f64628
comioko <comioko@outlook.com>
2026-07-25 10:56:44 +0800

    feat: 智能体日志

---
+++ src/main/java/github/comioko/articlepilot/mapper/AgentLogMapper.java
@@ -0,0 +1,14 @@
+package github.comioko.articlepilot.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import github.comioko.articlepilot.model.entity.AgentLog;
+
+
+/**
+ * 智能体日志 Mapper
+ *
+ * @author <a href="https://codefather.cn">编程导航学习圈</a>
+ */
+public interface AgentLogMapper extends BaseMapper<AgentLog> {
+
+}
