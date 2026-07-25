d5b5b813a0ffd5d14c3a3a8c94f415c87fe24d08
comioko <comioko@outlook.com>
2026-07-25 18:43:48 +0800

    feat: 统计分析

---
+++ src/main/java/github/comioko/articlepilot/service/StatisticsService.java
@@ -0,0 +1,19 @@
+package github.comioko.articlepilot.service;
+
+
+import github.comioko.articlepilot.model.vo.StatisticsVO;
+
+/**
+ * 统计服务
+ *
+ * @author <a href="https://codefather.cn">编程导航学习圈</a>
+ */
+public interface StatisticsService {
+
+    /**
+     * 获取系统统计数据
+     *
+     * @return 统计数据
+     */
+    StatisticsVO getStatistics();
+}
